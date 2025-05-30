67f77041671501f5744a7b91179ed3b19ab83d3a
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-05-30 10:54:31 -0400

    Actualiza la configuración de Next.js para permitir un nuevo origen de desarrollo y modifica la acción de verificación en el componente Verify.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['static.usernames.app-backend.toolsforhumanity.com'],
   },
-  allowedDevOrigins: ['*'], // Add your dev origin here
+  allowedDevOrigins: ['2a13-213-55-220-212.ngrok-free.app/'], // Add your dev origin here
   reactStrictMode: false,
 };
 
